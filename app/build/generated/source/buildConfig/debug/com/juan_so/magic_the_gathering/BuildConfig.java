7a945e1b5652115dc4715763143043ec8432f4c3
Juan M. Altamirano <juanmaalt@gmail.com>
2018-09-02 21:04:01 -0300

    Add files via upload

---
+++ app/build/generated/source/buildConfig/debug/com/juan_so/magic_the_gathering/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.juan_so.magic_the_gathering;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.juan_so.magic_the_gathering";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
